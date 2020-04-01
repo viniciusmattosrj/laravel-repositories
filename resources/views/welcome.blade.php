33e211d39b8f75458e35542a1e30ac7667eafe52
Vinicius Mattos <mvinicius@funcionalcorp.com.br>
2020-04-01 00:30:16 -0300

    PHP01042020 - Update message welcome.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Repositories
                 </div>
 
                 <div class="links">
